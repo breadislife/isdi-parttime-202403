eca755cf8486c14de4cc2431fb35384be3d015f5
danielhernandez <tarnegolet@hotmail.com>
2025-02-07 02:20:20 -0600

    implemented follow playlist service and spec #175

--- staff/daniel-hernandez/project/api/services/index.js
+++ staff/daniel-hernandez/project/api/services/index.js
@@ -1,25 +1,35 @@
 import login from './auth/login.js';
 import register from './auth/register.js';
 import checkEmail from './auth/checkEmail.js';
+
 import log from './log.js';
 import stream from './stream.js';
 import query from './query.js';
 import player from './player.js';
+
 import followUser from './user/followUser.js';
 import getUserInfo from './user/getUserInfo.js';
+
 import getPlaylistInfo from './playlist/getPlaylistInfo.js';
+import followPlaylist from './playlist/followPlaylist.js';
+
 import getAlbumInfo from './album/getAlbumInfo.js';
 
 export default {
    login,
    register,
    checkEmail,
+
    log,
    stream,
    query,
    player,
+
    followUser,
    getUserInfo,
+
    getPlaylistInfo,
+   followPlaylist,
+
    getAlbumInfo
 };
